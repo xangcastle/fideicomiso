09938c4138347bc712267effe704bf1359497eb7
sebastian.norena.marquez <sebastian.norena.marquez@gmail.com>
2018-01-09 18:32:11 -0500

    add:correcion compilacion

--- app/src/main/java/com/deltacopiers/banpro/fideicomiso/dashboard.java
+++ app/src/main/java/com/deltacopiers/banpro/fideicomiso/dashboard.java
@@ -23,9 +23,6 @@ public class dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
